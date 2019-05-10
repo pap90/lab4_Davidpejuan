62337afd36bd5763bb8a39d7495273c5840d1f96
pap90 <davidpejuan@unitec.edu>
2019-05-10 13:57:02 -0600

    push inicial

---
+++ lab4_davidpejuan.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){=
+	string cadena=NULL;
+	cout<< "Ingrese la cadena inicial: ";
+	cin >> cadena;
+	cout<<endl;
+
+}
---
+++ makefile
@@ -0,0 +1,5 @@
+lab4: lab4_davidpejuan.o
+	g++ lab4_davidpejuan.o -o lab4
+
+lab4_davidpejuan.o:	lab4_davidpejuan.cpp
+	g++ -c lab4_davidpejuan.cpp
