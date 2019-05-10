325f525070c695f2fd3e3501b7d320d7828297d6
pap90 <davidpejuan@unitec.edu>
2019-05-10 15:52:03 -0600

    limpieza del codigo

--- lab4_davidpejuan.cpp
+++ lab4_davidpejuan.cpp
@@ -19,7 +19,6 @@ int main(){
 	cout<<endl;
 	int x,y;
 	x=cadena.size();
-	//cout<<x;
 	cout<<"Ingrese el alto de la matriz: ";
 	cin >> y;
 	cout<<endl;
@@ -28,7 +27,6 @@ int main(){
 	for(int i=0;i<x;i++){
 		mat[0][i]=cadena[i];
 	}
-	//printmat(mat,x,y);
 	llenar(mat,x,y);
 	printmat(mat,x,y);
 	
@@ -63,12 +61,6 @@ char** llenar(char** mat,int x,int y){
 	for (int i=1;i<y;i++){
 		char* arreglo=mat[i-1];
 		for(int j=0;j<x;j++){
-			//cout<< "["<<arreglo[j]<<"]";
-			/*if(j>1 && j+2>=x){
-				if(arreglo[j]=='^'&&arreglo[j+1]=='^'&&arreglo[j+2]){
-					
-				}
-			}*/
 			if(j==0&&j<x){
 				if(arreglo[j]=='^'&&arreglo[j+1]=='^'){
 					mat[i][j]='^';
